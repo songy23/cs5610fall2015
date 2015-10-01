cffba4521171241904e5bd0dcb1195d798c08982
songy23 <songy23@ccs.neu.edu>
2015-09-30 18:46:55 -0700

    Change the page layout using the dashboard template

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
-    var app = express();
-    app.use(express.static(__dirname + '/public'));
-    var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-    var port = process.env.OPENSHIFT_NODEJS_PORT || 8080; 
-    app.listen(port, ipaddress);
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
+app.listen(port, ipaddress);
+app.get("/", function (req, res) {
+    res.sendfile(__dirname + '/public/assignment/home.html');
+});
