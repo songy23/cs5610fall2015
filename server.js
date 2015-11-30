3a41906d27bff524cb03a92d3f1afabddf090f4f
songy23 <songy23@ccs.neu.edu>
2015-11-29 22:36:46 -0800

    Modify server.js

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs5610');
+var connectionString = 'mongodb://localhost/cs5610';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+mongoose.connect(connectionString);
 var db = mongoose.connection;
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
